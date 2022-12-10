c958c4f32c770800093bd576b413b753c381dc95
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2022-12-10 17:35:54 +0200

    3 add org.springframework.boot.Banner

--- src/main/java/com/miroshnicheko/springbootapp/SpringBootAppApplication.java
+++ src/main/java/com/miroshnicheko/springbootapp/SpringBootAppApplication.java
@@ -7,9 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication app =
-                new SpringApplication(SpringBootSimpleApplication.class);
-        // put additional settings here
+        SpringApplication app = new SpringApplication
+                (SpringBootSimpleApplication.class);
+        app.setBanner(new Banner() {
+            @Override
+            public void printBanner(Environment environment,
+                                    Class<?> sourceClass, PrintStream out) {
+                out.print("\n\n\tThis is my own banner!\n\n".toUpperCase());
+            }
+        });
         app.run(args);
     }
 
