484f0ada430176b658c2adac275e3c92858ea610
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2022-12-13 12:30:20 +0200

    13 BootRun cmd args and containsOption

--- src/main/java/com/miroshnicheko/springbootapp/SpringBootAppApplication.java
+++ src/main/java/com/miroshnicheko/springbootapp/SpringBootAppApplication.java
@@ -1,14 +1,18 @@
 package com.miroshnicheko.springbootapp;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class SpringBootAppApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication
-                (SpringBootSimpleApplication.class);
+                (SpringBootAppApplication.class);
         app.setBanner(new Banner() {
             @Override
             public void printBanner(Environment environment,
