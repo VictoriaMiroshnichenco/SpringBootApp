06f1404c10d42b601a714ec9e6f9f7ceb3538374
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2022-12-15 22:31:47 +0200

    13 How to use @Value from application.properties and demo of ( constructor runs before @Value)

--- src/main/java/com/miroshnicheko/springbootapp/MyComponent.java
+++ src/main/java/com/miroshnicheko/springbootapp/MyComponent.java
@@ -3,6 +3,7 @@ package com.miroshnicheko.springbootapp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +13,12 @@ import java.util.List;
 public class MyComponent {
   //  private static final Logger log = LoggerFactory.getLogger
   //          (MyComponent.class);
+
+    @Value("${server.name}")
+    String serverName;
     @Autowired
     public MyComponent(ApplicationArguments args) {
-        boolean enable = args.containsOption("enable");
-        List<String> myArgs = args.getNonOptionArgs();
-
-
-        System.out.println("## > Are you  enabled" + enable);
-        System.out.println("## > Are you  enabled" + myArgs);
-        if(!myArgs.isEmpty())
-            myArgs.forEach(a -> System.out.println(a));
+        System.out.println("====from constructor MyComponent =====> serverName = " +serverName);
     }
 
 
--- src/main/java/com/miroshnicheko/springbootapp/SpringBootAppApplication.java
+++ src/main/java/com/miroshnicheko/springbootapp/SpringBootAppApplication.java
@@ -4,16 +4,20 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootAppApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder()
+        ConfigurableApplicationContext context =  new SpringApplicationBuilder()
                // .bannerMode(Banner.Mode.OFF)
                 .sources(SpringBootAppApplication.class)
                 .run(args);
+       MyComponent component = (MyComponent) context.getBean("myComponent");
         System.out.println("## > App Started!");
+        System.out.println("====== from main get bean get field ======> component.serverName =" + component.serverName);
+
     }
 
 }
