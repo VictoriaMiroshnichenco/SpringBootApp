9332b657993ad749ac7f00f1e590c7bdd387e320
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2022-12-19 15:13:54 +0200

    16 Properties ORDER: how to use PREFIX for MyAppProperties for  application.properties

--- src/main/java/com/miroshnicheko/springbootapp/MyComponent.java
+++ src/main/java/com/miroshnicheko/springbootapp/MyComponent.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
--- src/main/java/com/miroshnicheko/springbootapp/SpringBootAppApplication.java
+++ src/main/java/com/miroshnicheko/springbootapp/SpringBootAppApplication.java
@@ -1,15 +1,16 @@
 package com.miroshnicheko.springbootapp;
 
-import org.springframework.boot.Banner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.Properties;
 
 @SpringBootApplication
 public class SpringBootAppApplication {
+    @Autowired
+    MyAppProperties props;
 
     public static void main(String[] args) {
         SpringApplication app =
@@ -24,6 +25,10 @@ public class SpringBootAppApplication {
         MyComponent component = (MyComponent) context.getBean("myComponent");
         System.out.println("## > App Started!");
         System.out.println("====== from main get bean get field ======> component.prop1 =" + component.prop1);
+        MyAppProperties props = (MyAppProperties) context.getBean("myAppProperties");
+        System.out.println("====== from main get bean get field ======> MyAppProperties.myappproperties =" + props.getMyappproperties());
+        OtherAppProperties otherprops = (OtherAppProperties) context.getBean("otherAppProperties");
+        System.out.println("====== from main get bean get field ======> MyAppProperties.myappproperties =" + otherprops.getMyappproperties());
 
     }
 
