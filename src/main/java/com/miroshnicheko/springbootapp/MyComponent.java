dc363e07aa271ffd0c6b2b8892078fbfc9a893c5
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2022-12-19 12:58:28 +0200

    16 Properties ORDER: how to use app.setDefaultProperties. How to use ConfigurableApplicationContext. Git how to edit commit massage test

--- src/main/java/com/miroshnicheko/springbootapp/MyComponent.java
+++ src/main/java/com/miroshnicheko/springbootapp/MyComponent.java
@@ -3,6 +3,7 @@ package com.miroshnicheko.springbootapp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +13,14 @@ import java.util.List;
 public class MyComponent {
   //  private static final Logger log = LoggerFactory.getLogger
   //          (MyComponent.class);
+
+
+    @Value("${prop1}")
+    String prop1;
     @Autowired
     public MyComponent(ApplicationArguments args) {
         boolean enable = args.containsOption("enable");
         List<String> myArgs = args.getNonOptionArgs();
-
-
         System.out.println("## > Are you  enabled" + enable);
         System.out.println("## > Are you  enabled" + myArgs);
         if(!myArgs.isEmpty())
--- src/main/java/com/miroshnicheko/springbootapp/SpringBootAppApplication.java
+++ src/main/java/com/miroshnicheko/springbootapp/SpringBootAppApplication.java
@@ -4,16 +4,27 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class SpringBootAppApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder()
-               // .bannerMode(Banner.Mode.OFF)
-                .sources(SpringBootAppApplication.class)
-                .run(args);
+        SpringApplication app =
+                new SpringApplication(SpringBootAppApplication.class);
+
+        // put additional settings here
+        Properties properties = new Properties();
+        properties.put("prop1","value1__from__app.setDefaultProperties(properties);");
+
+        app.setDefaultProperties(properties);
+        ConfigurableApplicationContext context = app.run(args);
+        MyComponent component = (MyComponent) context.getBean("myComponent");
         System.out.println("## > App Started!");
+        System.out.println("====== from main get bean get field ======> component.prop1 =" + component.prop1);
+
     }
 
 }
