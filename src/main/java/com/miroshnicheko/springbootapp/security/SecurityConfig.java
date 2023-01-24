58fcab8727a33737b83566a8ddd160d92609bbc6
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2023-01-24 15:47:24 +0200

    37 Security: httpBasic in memory

---
+++ src/main/java/com/miroshnicheko/springbootapp/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.miroshnicheko.springbootapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.
+        builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.
+        HttpSecurity;
+
+import org.springframework.security.config.annotation.web.configuration.
+        WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("vika")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN","USER");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .httpBasic();
+    }
+}
